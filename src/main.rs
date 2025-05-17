b145cc219379ddb6b09e20bb1349532533731430
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2025-05-17 16:52:07 +0700

    chore: remove smell code

--- src/main.rs
+++ src/main.rs
@@ -19,10 +19,7 @@ lazy_static! {
         lib
     };
 }
-// Type definitions for the DLL functions
-// type FindProcessIdByNameRaw = unsafe extern "C" fn(*const u16) -> u32;
-// type FindMainWindowByPID = unsafe extern "C" fn(u32) -> HWND;
-// type SetRandomTitle = unsafe extern "C" fn(HWND, *const u16);
+
 type RenameWindowTitleFn =
     unsafe extern "C" fn(target_process_name: *const u16, new_window_title: *const u16);
 
@@ -32,26 +29,15 @@ slint::include_modules!();
 
 fn main() -> Result<(), Box<dyn Error>> {
     let version = env!("CARGO_PKG_VERSION");
-
-    // Load the functions
-    // let find_process_id: Symbol<FindProcessIdByNameRaw> =
-    //     unsafe { LLIB.get(b"FindProcessIdByNameRaw")? };
-    // let find_main_window: Symbol<FindMainWindowByPID> =
-    //     unsafe { LLIB.get(b"FindMainWindowByPID")? };
-    // let set_random_title: Symbol<SetRandomTitle> = unsafe { LLIB.get(b"SetRandomTitle")? };
-
     let rename_windows_title: Symbol<RenameWindowTitleFn> =
         unsafe { LLIB.get(b"RenameWindowTitleByProcessName")? };
 
     let running = Arc::new(AtomicBool::new(true));
     let flag = Arc::clone(&running);
-
     let ui = AppWindow::new()?;
     ui.set_version(version.to_string().into());
     let model = Rc::new(slint::VecModel::default());
-
     model.set_vec(randomizer::get_processes_names().unwrap_or_default());
-
     ui.set_processes(model.clone().into());
 
     {
@@ -65,18 +51,6 @@ fn main() -> Result<(), Box<dyn Error>> {
                 .encode_wide()
                 .chain(std::iter::once(0)) // Null-terminate
                 .collect();
-            // let pid = unsafe { find_process_id(process_name_w.as_ptr()) };
-
-            // if pid == 0 {
-            //     return;
-            // }
-
-            // let hwnd = unsafe { find_main_window(pid) };
-            // if hwnd.is_null() {
-            //     return;
-            // }
-            // Cast HWND to isize for thread safety
-            // let hwnd_val = hwnd as isize;
             let rename_windows_title = rename_windows_title.clone();
             let flag = flag.clone();
             thread::spawn(move || {
@@ -87,7 +61,6 @@ fn main() -> Result<(), Box<dyn Error>> {
                         .encode_wide()
                         .chain(std::iter::once(0)) // Null-terminate
                         .collect();
-                    // unsafe { set_random_title(hwnd, new_title_w.as_ptr()) };
                     unsafe { rename_windows_title(process_name_w.as_ptr(), new_title_w.as_ptr()) };
                     thread::sleep(std::time::Duration::from_millis(100));
                 }
